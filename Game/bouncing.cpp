f4de599f591fdb13a5d35af1c12394c457e1afad
SIL1202 <167217913+SIL1202@users.noreply.github.com>
2025-05-22 04:53:01 +0800

    feat: Game/Makefile, bouncing.cpp - Add new bouncing balls with physics

--- Game/bouncing.cpp
+++ Game/bouncing.cpp
@@ -9,13 +9,15 @@
 class Ball {
 public:
   Ball() {}
-  Ball(Vector2 p, float r, Color c) : pos(p), r(r), color(c), velocity(0.f) {}
-
-  void draw() const { DrawCircle(pos.x, pos.y, r, color); }
+  Ball(Vector2 p, float r, Color c) : pos(p), r(r), color(c) {
+    float angle = GetRandomValue(0, 360) * DEG2RAD;
+    float speed = GetRandomValue(100, 300);
+    v = {cosf(angle) * speed, sinf(angle) * speed};
+  }
 
   static void connect(const Ball &b1, const Ball &b2) {
     Vector2 dir = Vector2Subtract(b2.pos, b1.pos);
-    float length = std::sqrt(dir.x * dir.x + dir.y * dir.y);
+    float length = Vector2Length(dir);
     if (length == 0)
       return;
 
@@ -24,23 +26,43 @@ public:
     Vector2 newS = {b1.pos.x + unitDir.x * b1.r, b1.pos.y + unitDir.y * b1.r};
     Vector2 newE = {b2.pos.x - unitDir.x * b2.r, b2.pos.y - unitDir.y * b2.r};
 
-    DrawLineEx(newS, newE, 10.f, RED);
+    float thickness = 3000.f / length;
+    thickness = Clamp(thickness, 1.f, 20.f);
+    DrawLineEx(newS, newE, thickness, RED);
   }
+
+  void update(float dt) {
+    pos.x += v.x * dt;
+    pos.y += v.y * dt;
+
+    if (pos.x - r <= 0 || pos.x + r >= WIDTH)
+      v.x *= -1;
+
+    if (pos.y - r <= 0 || pos.y + r >= HEIGHT)
+      v.y *= -1;
+  }
+
+  void draw() const { DrawCircle(pos.x, pos.y, r, color); }
 
 private:
   Vector2 pos;
   float r;
   Color color;
-  float velocity;
+  Vector2 v;
 };
 
 int main() {
 
   InitWindow(WIDTH, HEIGHT, "BOUNCING");
+  SetTargetFPS(60);
+
   Ball ball1({200, 400}, 50, BLACK);
   Ball ball2({500, 200}, 50, BLACK);
   while (!WindowShouldClose()) {
+    float dt = GetFrameTime();
     BeginDrawing();
+    ball1.update(dt);
+    ball2.update(dt);
 
     ClearBackground(WHITE);
 
